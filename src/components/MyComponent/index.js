eec97a8c14999376c93e89c1ae3ce4ef6fe65230
someuser <someuser@gmail.com>
2020-12-16 09:40:45 +0000

    first commit

---
+++ src/components/MyComponent/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+
+const MyComponent = (props) => (
+  <div>
+    <b><Text field={props.fields.heading} /></b>
+	<br/>
+	
+	<Text field={props.fields.Description} />
+	
+  </div>
+);
+
+export default MyComponent;
